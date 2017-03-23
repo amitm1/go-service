571815b2d9888bf0c66280539d2f56c5cb9d0035
Amit Mishra <amit.mishra@fox.com>
2017-03-22 17:50:03 -0700

    Trying travis with glide

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+	"fmt"
 
-	"github.com/Sirupsen/logrus",
+	log "github.com/Sirupsen/logrus"
 
 	"github.com/amitm1/go-service/cmd"
 )
 
 func main() {
-	//fmt.Printf("Hello, world.\n")
+	fmt.Printf("Hello, world.\n")
 	fmt.Printf(cmd.Reverse("Hello, world Amit Mishra.\n"))
 
-	logrus.WithFields(log.Fields{
+	log.WithFields(log.Fields{
 		"animal": "walrus",
 	}).Info("A walrus appears")
 
